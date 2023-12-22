9344f5744d08da30a0865722449a9ff8a51e64d9
Dinithma <tharusha.dinithma@gmail.com>
2023-12-22 16:53:56 +0530

    minor changes

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -11,7 +11,7 @@ export const Gallery = () => {
   const h1StyleRegular = {
     fontFamily: "sans-serif",
     fontSize: "48px",
-    fontWeight: 100,
+    fontWeight: 400,
     lineHeight: "64px",
     letterSpacing: "0em",
     textAlign: "left",
@@ -19,11 +19,6 @@ export const Gallery = () => {
     color: "black",
   };
 
-  const h3StyleRegular = {
-    fontFamily: "sans-serif",
-    fontWeight: 80,
-    lineHeight: "34px",
-  };
 
   const boldWordStyle = {
     fontFamily: "sans-serif",
@@ -33,43 +28,22 @@ export const Gallery = () => {
     color: "black",
   };
 
-  const buttonStyle = {
-    marginTop: "60px",
-    backgroundColor: isHovered ? "black" : "white",
-    color: isHovered ? "white" : "black",
-    border: "2px solid black",
-    borderRadius: "30px",
-    padding: "10px 40px",
-    cursor: "pointer",
-    transition: "background-color 0.3s, color 0.3s",
-  };
-
   return (
     <div className="container">
-      <Row style={row}>
-        <Col  md={4}>
-          <Col md={11}>
-          <h3 style={boldWordStyle}>
-           CAREERS
-            </h3>
-            <h1 style={h1StyleRegular}>
-              Be a part of <br /> our story <br /> 
-              {/* {" "}
+      <Row>
+        <Col md={11}>
+          <h3 style={boldWordStyle}>OUR WORK</h3>
+          <h1 style={h1StyleRegular}>
+            vital component of global transportation, connecting people and
+            goods
+            {/* {" "}
               <span style={boldWordStyle}>embedded finance engine</span> */}
-            </h1>
-          </Col>
-          <Col md={11}>
-            <h3 style={h3StyleRegular}>
-            At Avero, we're always looking for creative problem solvers to join our team
-            </h3>
-            <Button
-              style={{ ...buttonStyle, ...h3StyleRegular }}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              Join Now
-            </Button>
-          </Col>
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ marginTop: "10px" }} md={4}>
+          <img src="img/img.png" className="img-responsive" alt="" />
         </Col>
         <Col style={{ marginTop: "10px" }} md={4}>
           <img src="img/img.png" className="img-responsive" alt="" />
