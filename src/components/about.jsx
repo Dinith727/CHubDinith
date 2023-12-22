db810b4256c128c2ea96bafa8fa0c5a56ab52e71
Dinithma <tharusha.dinithma@gmail.com>
2023-12-22 09:42:40 +0530

    minor changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,10 +5,7 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/climate-change-app.png" className="img-responsive" alt="" />{" "}
-          </div>
+          
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>About Us</h2>
@@ -35,6 +32,10 @@ export const About = (props) => {
                 </div>
               </div> */}
             </div>
+          </div>
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/climate-change-app.png" className="img-responsive" alt="" />{" "}
           </div>
         </div>
       </div>
