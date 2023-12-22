d618027b36bb9c5ca321ddf70ad969e0cfd2816b
Dinithma <tharusha.dinithma@gmail.com>
2023-12-22 22:17:34 +0530

    minor updates

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -26,6 +26,33 @@ export const OurWork = () => {
     color: "black",
   };
 
+
+  const containerStyle = {
+    position: "relative",
+    overflow: "hidden",
+    marginTop: "10px",
+  };
+
+  const imageStyle = {
+    width: "100%",
+    height: "auto",
+    transition: "filter 0.3s ease-in-out, margin 0.3s ease-in-out", // Add margin transition
+  };
+
+  const overlayStyle = {
+    position: "absolute",
+    bottom: "0",
+    left: "0",
+    width: "100%",
+    padding: "10px", // Adjust padding as needed
+    background: "linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%)", // Use a gradient for a smooth transition
+    color: "white",
+    opacity: "0",
+    transition: "opacity 0.3s ease-in-out, transform 0.3s ease-in-out", // Add transform transition
+    transform: "translateY(100%)", // Initial position below the container
+    textAlign: "center",
+  };
+
   return (
     <div style={row} className="container">
       <Row>
@@ -40,16 +67,32 @@ export const OurWork = () => {
         </Col>
       </Row>
       <Row>
-        <Col style={{ marginTop: "10px" }} md={4}>
-          <img src="img/img.png" className="img-responsive" alt="" />
-        </Col>
-        <Col style={{ marginTop: "10px" }} md={4}>
-          <img src="img/img.png" className="img-responsive" alt="" />
-        </Col>
-        <Col style={{ marginTop: "10px" }} md={4}>
-          <img src="img/img.png" className="img-responsive" alt="" />
-        </Col>
-      </Row>
+      <Col style={containerStyle} md={4}>
+        <div
+          style={containerStyle}
+          onMouseOver={(e) => {
+            e.currentTarget.querySelector(".overlay").style.opacity = "1";
+            e.currentTarget.querySelector(".overlay").style.transform = "translateY(0)";
+            e.currentTarget.querySelector(".img-responsive").style.filter =
+              "brightness(0.7)";
+            e.currentTarget.querySelector(".img-responsive").style.marginBottom = "-10px"; // Adjust margin for flush transition
+          }}
+          onMouseOut={(e) => {
+            e.currentTarget.querySelector(".overlay").style.opacity = "0";
+            e.currentTarget.querySelector(".overlay").style.transform = "translateY(100%)";
+            e.currentTarget.querySelector(".img-responsive").style.filter =
+              "brightness(1)";
+            e.currentTarget.querySelector(".img-responsive").style.marginBottom = "0";
+          }}
+        >
+          <img src="img/img.png" className="img-responsive" style={imageStyle} alt="" />
+          <div style={overlayStyle} className="overlay">
+            <p>Your Text Here</p>
+          </div>
+        </div>
+      </Col>
+      {/* Repeat the above structure for other images */}
+    </Row>
     </div>
   );
 };
