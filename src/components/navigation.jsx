6f2aa34e1b0f69c77cebcc1ca949e7b1c79afe69
Uvindushamodya <uvinduabeykoon123@gmail.com>
2023-12-23 09:34:32 +0530

    navbar transition

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
-import { Row, Col, Button } from "react-bootstrap";
-
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 
 export const Navigation = (props) => {
-  const [isHovered, setIsHovered] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
   const buttonStyle = {
-    marginTop:"40px",
-    backgroundColor: isHovered ? 'black' : 'white',
-    color: isHovered ? 'white' : 'black',
+    marginTop: "40px",
+    backgroundColor: scrolled ? 'black' : 'transparent',
+    color: scrolled ? 'white' : 'black',
     border: '2px solid black',
     borderRadius: '30px',
     padding: '10px 40px',
@@ -16,11 +15,31 @@ export const Navigation = (props) => {
     transition: 'background-color 0.3s, color 0.3s',
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        if (!scrolled) {
+          setScrolled(true);
+        }
+      } else {
+        if (scrolled) {
+          setScrolled(false);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
   return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+    <nav id="menu" className={`navbar navbar-default navbar-fixed-top ${scrolled ? 'solid' : ''}`}>
       <div className="container">
         <div className="navbar-header">
-          <button
+        <button
             type="button"
             className="navbar-toggle collapsed"
             data-toggle="collapse"
@@ -37,15 +56,8 @@ export const Navigation = (props) => {
           </a>{" "}
         </div>
 
-        <div
-          
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul
-            className="nav navbar-nav navbar-left"
-            style={{ marginLeft: "50px" }}
-          >
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-left" style={{ marginLeft: "50px" }}>
             <li>
               <a href="#features" className="page-scroll">
                 About
@@ -66,14 +78,15 @@ export const Navigation = (props) => {
                 Resources
               </a>
             </li>
+            {/* Uncomment the following lines if you want to include the button in the navigation */}
             {/* <li>
-            <Button
-               style={buttonStyle}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              Learn More
-            </Button>
+              <Button
+                style={buttonStyle}
+                onMouseEnter={() => setScrolled(true)}
+                onMouseLeave={() => setScrolled(false)}
+              >
+                Learn More
+              </Button>
             </li> */}
           </ul>
         </div>
